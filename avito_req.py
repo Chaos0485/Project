035c02e6c135fc13b0e0eed1a2a99ed3b1a95918
Chaos <vanianesibayan@yandex.ru>
2018-01-29 21:25:55 +0600

    Забыл про функцию, добавил

--- avito_req.py
+++ avito_req.py
@@ -2,7 +2,7 @@ import requests
 
 def get_html(url):
 	try:
-		result = requests.get(url, params={'q': 'геймпад'})
+		result = requests.get(url, params={'q': 'macbook'})
 		result.raise_for_status()
 		return result.text
 	except requests.exceptions.RequestException as e:
--- parse_avito.py
+++ parse_avito.py
@@ -7,11 +7,14 @@ bs = BeautifulSoup(html, 'html.parser')
 
 avito_sale = {}
 
-for advertisement in bs.find_all('div', class_='item_table'):
-	title = advertisement.find('a', class_='item-description-title-link')
-	price = advertisement.find('div', class_='about').contents[0]
-	avito_sale['title'] = title.get('title')
-	avito_sale['price'] = price.strip()
-	avito_search = []
-	avito_search.append(avito_sale)
-	print(avito_search)
+def avito_parsing():
+	for advertisement in bs.find_all('div', class_='item_table'):
+		title = advertisement.find('a', class_='item-description-title-link')
+		price = advertisement.find('div', class_='about').contents[0]
+		avito_sale['title'] = title.get('title')
+		avito_sale['price'] = price.strip()
+		avito_search = []
+		avito_search.append(avito_sale)
+		result = avito_search
+		return result
+avito_parsing()
